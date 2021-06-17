0c8ba8b7ac4546d22f3e1e4826da9f36a7a67412
James Kmet <270028217@bun2-206-21.acg.ac.nz>
2021-06-17 12:46:22 +1200

    thursday

--- js/custom.js
+++ js/custom.js
@@ -80,6 +80,8 @@ getHotels.onclick = function() {
 
 // booking caluclators
 
+// booking caluclator for hotel One
+
 function nightValidatorOne() {
   var x, text;
   // Get the value of the input field with id="x"
@@ -87,14 +89,13 @@ function nightValidatorOne() {
   // If x is Not a Number or less than one or greater than 10
   if (isNaN(x) || x < 1 || x > 5) {
     text = "Sorry but you are only able to book a maximum of 5 nights";
+    document.getElementById('step2One').style.display = "none";
   } else {
     text = "This slot is available. Please confirm details below";
+    document.getElementById('step2One').style.display = "contents";
   }
-  document.getElementById("subMessage").innerHTML = text;
+  document.getElementById("subMessageOne").innerHTML = text;
 }
-
-
-
 
 function caluclatorValidatorOne() {
   var getHotelModalNightsOne = document.getElementById('hotelModalNightsOne').value;
@@ -102,13 +103,98 @@ function caluclatorValidatorOne() {
   var y = 157;
   var z = x * y;
   text = z;
-
-  document.getElementById("outPutTwo").innerHTML = text;
+  document.getElementById("outPutOne").innerHTML = text;
 }
 nightValidatorOne();
 caluclatorValidatorOne();
 
 
+// booking caluclator for hotel two
+
+function nightValidatorTwo() {
+  var x, text;
+  // Get the value of the input field with id="x"
+  x = document.getElementById("hotelModalNightsTwo").value;
+  // If x is Not a Number or less than one or greater than 10
+  if (isNaN(x) || x < 1 || x > 10) {
+    text = "Sorry but you are only able to book a maximum of 10 nights";
+    document.getElementById('step2Two').style.display = "none";
+  } else {
+    text = "This slot is available. Please confirm details below";
+    document.getElementById('step2Two').style.display = "contents";
+  }
+  document.getElementById("subMessageTwo").innerHTML = text;
+}
+
+function caluclatorValidatorTwo() {
+  var getHotelModalNightsOne = document.getElementById('hotelModalNightsTwo').value;
+  var x = getHotelModalNightsOne;
+  var y = 30;
+  var z = x * y;
+  text = z;
+  document.getElementById("outPutTwo").innerHTML = text;
+}
+nightValidatorTwo();
+caluclatorValidatorTwo();
+
+
+// booking caluclator for hotel Three
+
+function nightValidatorThree() {
+  var x, text;
+  // Get the value of the input field with id="x"
+  x = document.getElementById("hotelModalNightsThree").value;
+  // If x is Not a Number or less than one or greater than 10
+  if (isNaN(x) || x < 3 || x > 10) {
+    text = "Sorry but you are only able to book a maximum of 10 nights";
+    document.getElementById('step2Three').style.display = "none";
+  } else {
+    text = "This slot is available. Please confirm details below";
+    document.getElementById('step2Three').style.display = "contents";
+  }
+  document.getElementById("subMessageThree").innerHTML = text;
+}
+
+function caluclatorValidatorThree() {
+  var getHotelModalNightsOne = document.getElementById('hotelModalNightsThree').value;
+  var x = getHotelModalNightsOne;
+  var y = 90;
+  var z = x * y;
+  text = z;
+  document.getElementById("outPutThree").innerHTML = text;
+}
+nightValidatorThree();
+caluclatorValidatorThree();
+
+
+
+// booking caluclator for hotel Four
+
+function nightValidatorFour() {
+  var x, text;
+  // Get the value of the input field with id="x"
+  x = document.getElementById("hotelModalNightsFour").value;
+  // If x is Not a Number or less than one or greater than 10
+  if (isNaN(x) || x < 3 || x > 10) {
+    text = "Sorry but you are only able to book a maximum of 10 nights";
+    document.getElementById('step2Four').style.display = "none";
+  } else {
+    text = "This slot is available. Please confirm details below";
+    document.getElementById('step2Four').style.display = "contents";
+  }
+  document.getElementById("subMessageFour").innerHTML = text;
+}
+
+function caluclatorValidatorFour() {
+  var getHotelModalNightsFour = document.getElementById('hotelModalNightsFour').value;
+  var x = getHotelModalNightsFour;
+  var y = 240;
+  var z = x * y;
+  text = z;
+  document.getElementById("outPutFour").innerHTML = text;
+}
+nightValidatorFour();
+caluclatorValidatorFour();
 
 
 
